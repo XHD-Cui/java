8e54dee80e9ccdbeb1c570c7c6f7ffea268dacae
cxy <a@lxxxjs.cn>
2019-02-26 22:27:03 +0800

    OOP多线程在线群聊+私聊

---
+++ QSChat/src/cn/lxxxjs/qschat/QChat.java
@@ -0,0 +1,136 @@
+package cn.lxxxjs.qschat;
+
+/**
+ * 封装使用多线程：chat
+	多人在线客户端
+	私聊
+ * 
+ */
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+public class QChat {
+	static private CopyOnWriteArraySet<Channel> all = new CopyOnWriteArraySet<>();
+	public static void main(String[] args) throws IOException {
+		System.out.println("-----server----");
+		ServerSocket server = new ServerSocket(8888);
+		
+		while(true) {
+			Socket client = server.accept();
+			System.out.println("一个客户端建立了链接");
+			Channel c = new Channel(client);
+			all.add(c);
+			new Thread(c).start();
+		}
+		
+		
+	}
+	
+	//一个客户代表一个channel
+	static class Channel implements Runnable
+	{
+		private DataInputStream dis; //输入流
+		private DataOutputStream dos; //输出流
+		private Socket client; //客户端
+		private boolean isRunning;
+		private String name;
+		public Channel(Socket client) {
+			this.client = client;
+			this.isRunning = true;
+			try {
+				dis = new DataInputStream(client.getInputStream());
+				dos = new DataOutputStream(client.getOutputStream());
+				this.name = receive();
+				send(name+",欢迎您！");
+				sendAll("系统消息："+name+"进入群聊",true);
+			} catch (IOException e) {
+				System.out.println("---dis,dos---");
+				release();
+				//e.printStackTrace();
+			}
+			
+		}
+		
+		//得到数据
+		private String receive() {
+			String meg = "";
+			try {
+				meg = dis.readUTF();
+			} catch (IOException e) {
+				System.out.println("---receive---");
+				release();
+			}
+			return meg;
+		}
+		//发送数据
+		private void send(String meg) {
+			try {
+				dos.writeUTF(meg);
+				dos.flush();
+			} catch (IOException e) {
+				System.out.println("---send---");
+				release();
+			}
+			
+		}
+		//发送数据
+		/**
+		 * 约定数据格式@name：meg
+		 * @param meg
+		 * @param isSys
+		 */
+		private void sendAll(String meg,boolean isSys) {
+			boolean isPrivate = meg.startsWith("@");
+			if(isPrivate){
+				int idx = meg.indexOf(":");
+				String targetname = meg.substring(1,idx);
+				String targetmeg = meg.substring(idx+1);
+				for(Channel other:all){
+					if(other.name.equals(targetname)) {
+						other.send(this.name+"悄悄地对您说:"+targetmeg);
+						break;
+					}
+				}
+			}else {
+				for(Channel other:all) {
+					if(other == this) {
+						continue;
+					}
+					if(isSys) {
+						other.send(meg);
+					}else {
+						other.send(this.name+":"+meg);
+					}
+					
+				}
+			}
+					
+		}
+		
+		//释放资源
+		private void release() {
+			this.isRunning = false;
+			Closeend.close(dis,dos,client);
+			all.remove(this);
+			sendAll("系统消息："+this.name+"离开了群聊",true);
+		}
+
+		@Override
+		public void run() {
+			while(isRunning) {
+				String meg = receive();
+				if(!meg.equals("")) {
+					sendAll(meg,false);
+				}
+			}
+			
+		}
+		
+	}
+
+}
---
+++ QSChat/src/cn/lxxxjs/qschat/Send.java
@@ -0,0 +1,76 @@
+package cn.lxxxjs.qschat;
+
+
+/**
+ * 封装使用多线程：发送端
+ */
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class Send implements Runnable{
+	private BufferedReader console;
+	private DataOutputStream dos;
+	private Socket client; //客户端
+	private boolean isRunning;
+	private String name;
+	public Send(Socket client,String name) {
+		this.client = client;
+		this.isRunning = true;
+		console = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			dos = new DataOutputStream(client.getOutputStream());
+			this.name = name;
+			send(name);
+		} catch (IOException e) {
+			// TODO 自动生成的 catch 块
+			System.out.println("===s===");
+			release();
+		}
+	}
+	
+	//发送消息
+	private void send(String meg) {
+		try {
+			dos.writeUTF(meg);
+			dos.flush();
+		} catch (IOException e) {
+			System.out.println("====ss===");
+			release();
+		}
+		
+	}
+	
+	
+	//释放资源
+	private void release() {
+		this.isRunning = false;
+		Closeend.close(dos,client);
+	}
+	
+	//从控制台获取消息
+	private String getStrFormConlose() {
+		try {
+			return console.readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return "";
+	}
+	
+	@Override
+	public void run() {
+		while(isRunning) {
+			String meg = getStrFormConlose();
+			if(!meg.equals("")) {
+				send(meg);
+			}
+		}
+		
+	}
+
+}
